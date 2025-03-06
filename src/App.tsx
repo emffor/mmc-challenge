d51ef667cc1ae488b81da452454e4852860f5415
eloanmf <eloanmf@hotmail.com>
2025-03-06 20:21:20 -0300

    ajustando AuthCheck para despachar a ação de verificação ao montar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import { AppRoutes } from './routes'
 import { ThemeProvider } from './context/ThemeContext'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { useEffect } from 'react'
+import { checkAuth } from './store/authSlice'
+import { useAppDispatch } from './hook/useTypedRedux'
+
+function AuthCheck({ children }: { children: React.ReactNode }) {
+  const dispatch = useAppDispatch();
+  
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+  
+  return <>{children}</>;
+}
 
 function App() {
   return (
     <Provider store={store}>
       <ThemeProvider>
-        <GlobalStyle />
-        <AppRoutes />
+        <AuthCheck>
+          <GlobalStyle />
+          <AppRoutes />
+        </AuthCheck>
       </ThemeProvider>
     </Provider>
   )
